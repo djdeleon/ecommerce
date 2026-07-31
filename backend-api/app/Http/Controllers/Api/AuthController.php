a00a28d26502c3db63a00341017e630d12acd932
david <djcdeleon24@gmail.com>
2026-07-31 18:56:26 +0800

    test: validate and store user in the database

--- backend-api/app/Http/Controllers/Api/AuthController.php
+++ backend-api/app/Http/Controllers/Api/AuthController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         $validateData = $request->validate([
+            'name'  => 'required|string|max:255',
             'email' => 'required|email',
             'password' => 'required|min:8'
         ]);
 
-        return response()->json(['message' => 'Validation passed.'], 200);
+        User::create([
+            'name' => $validateData['name'],
+            'email' => $validateData['email'],
+            'password' => Hash::make($validateData['password'])
+        ]);
+
+        return response()->json(['message' => 'Registration Successful.'], 201);
     }
 }
--- backend-api/tests/Feature/AuthenticationTest.php
+++ backend-api/tests/Feature/AuthenticationTest.php
@@ -1,8 +1,33 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+
+uses(RefreshDatabase::class);
+
 test('it returns validation errors if registration fields are missing', function () {
     $response = $this->postJson(route('api.register'), []);
 
     $response->assertStatus(422)
-            ->assertJsonValidationErrors(['email', 'password']);
+            ->assertJsonValidationErrors(['name', 'email', 'password']);
+});
+
+test('it saves a user to the database and hashes their password upon successful registration', function () {
+    $user = [
+        'name'     => 'david',
+        'email'    => 'customer@example.com',
+        'password' => 'securePassword123'
+    ];
+
+    $response = $this->postJson(route('api.register'), $user);
+
+    $response->assertStatus(201)
+            ->assertJsonPath('message', 'Registration Successful.');
+
+    $this->assertDatabaseHas('users', [
+        'email' => $user['email']
+    ]);
+
+    $user = User::where('email', $user['email'])->first();
+    expect($user->password)->not->toBe($user['email']);
 });
