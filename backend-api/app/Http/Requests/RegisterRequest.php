0feee6116da10b4d4e70c08e17ef3c8f85df35a6
david <djcdeleon24@gmail.com>
2026-07-31 20:09:20 +0800

    test: enforce unique email validation during user registration

--- backend-api/app/Http/Requests/RegisterRequest.php
+++ backend-api/app/Http/Requests/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name'  => 'required|string|max:255',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required|min:8'
         ];
     }
--- backend-api/tests/Feature/AuthenticationTest.php
+++ backend-api/tests/Feature/AuthenticationTest.php
@@ -5,14 +5,14 @@ use App\Models\User;
 
 uses(RefreshDatabase::class);
 
-test('it returns validation errors if registration fields are missing', function () {
+it('returns validation errors if registration fields are missing', function () {
     $response = $this->postJson(route('api.register'), []);
 
     $response->assertStatus(422)
             ->assertJsonValidationErrors(['name', 'email', 'password']);
 });
 
-test('it saves a user to the database and hashes their password upon successful registration', function () {
+it('saves a user to the database and hashes their password upon successful registration', function () {
     $user = [
         'name'     => 'david',
         'email'    => 'customer@example.com',
@@ -30,4 +30,21 @@ test('it saves a user to the database and hashes their password upon successful 
 
     $user = User::where('email', $user['email'])->first();
     expect($user->password)->not->toBe($user['email']);
+});
+
+it('returns an error if the given email already exists', function () {
+    User::factory()->create([
+        'email' => 'duplicate@example.com'
+    ]);
+
+    $user = [
+        'name'     => 'david',
+        'email'    => 'duplicate@example.com',
+        'password' => 'securePassword123'
+    ];
+
+    $response = $this->postJson(route('api.register'), $user);
+
+    $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
 });
