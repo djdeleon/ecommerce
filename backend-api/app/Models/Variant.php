d404b8170cfcd9bb2c4f78db7dfbcdadd690739e
david <djcdeleon24@gmail.com>
2026-08-06 22:24:29 +0800

    test: add sku unique test

--- backend-api/app/Models/Variant.php
+++ backend-api/app/Models/Variant.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use Database\Factories\VariantFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Variant extends Model
 {
+    /** @use HasFactory<VariantFactory> */
+    use HasFactory;
+
     protected $fillable = [
         'sku',
         'price',
     ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- backend-api/tests/Feature/VariantTest.php
+++ backend-api/tests/Feature/VariantTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Models\Variant;
 
 test('a product can have variants', function () {
     $product = Product::factory()->create();
@@ -15,6 +16,16 @@ test('a product can have variants', function () {
         ->assertCreated();
 })->only();
 
-// test('creating a producting variant with same SKU is not allowed', function () {
+test('creating a producting variant with same SKU is not allowed', function () {
+    $variantA = Variant::factory()->create();
+    $variantB = [
+        'product_id' => $variantA->product->id,
+        'sku' => $variantA->sku,
+        'price' => 100.00,
+    ];
 
-// });
+    $this->actingAs($variantA->product->vendor->user, 'sanctum')
+        ->postJson(route('variants.store', $variantA->product), $variantB)
+        ->assertUnprocessable()
+        ->assertJsonValidationErrors(['sku']);
+})->only();
